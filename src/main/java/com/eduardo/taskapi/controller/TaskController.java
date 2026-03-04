77d9689e51878eaba5376833719740e20d116542
Edu <dudualbring@gmail.com>
2026-03-04 11:49:18 -0300

    Criação tasks.

---
+++ src/main/java/com/eduardo/taskapi/controller/TaskController.java
@@ -0,0 +1,22 @@
+package com.eduardo.taskapi.controller;
+
+import com.eduardo.taskapi.model.Task;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.eduardo.taskapi.repository.TaskRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Task>> findAll() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+}
---
+++ src/main/java/com/eduardo/taskapi/model/Task.java
@@ -0,0 +1,15 @@
+package com.eduardo.taskapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+
+}
